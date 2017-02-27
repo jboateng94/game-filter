9bc17b19e2813c6ab23b4fe00a052919b7668bdf
Jeremy Boateng <jeremyba@spartaglobal.co>
2017-02-27 13:46:05 +0000

    connected to the database and added access to (soon to be) front-end code

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ var routes = require('./config/routes');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose = require('mongoose');
+
+// connect to the database
+mongoose.connect('mongodb://localhost/posts');
