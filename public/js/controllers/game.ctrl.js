c85c846e32e524b4e7184aa78b01eab996388159
Jeremy Boateng <jeremyba@spartaglobal.co>
2017-03-01 17:32:55 +0000

    search items now appear on the app underneath the search bar

--- public/js/controllers/game.ctrl.js
+++ public/js/controllers/game.ctrl.js
@@ -15,11 +15,11 @@ function gameController(Game) {
 		  .then(function successCallback(response) {
 			  // this callback will be called asynchronously
 		      // when the response is available
-				
-			  self.data = JSON.parse(response.data)
-			  for (var i = 0; i < self.data.results.length; i++) {
-			  	console.log(self.data.results[i]);
-			  }
+			
+			  self.data = JSON.parse(response.data).results
+			  // for (var i = 0; i < self.data.results.length; i++) {
+			  // 	console.log(self.data.results[i]);
+			  // }
 			}, function errorCallback(response) {
 		    // called asynchronously if an error occurs
 		    // or server returns response with an error status.
