c4af332e613fa3a5d89d06c687a3c1d9f7716647
Jeremy Boateng <jeremyba@spartaglobal.co>
2017-03-03 10:03:34 +0000

    removed console logs from games.js

--- controllers/games.js
+++ controllers/games.js
@@ -9,19 +9,14 @@ function indexGame(req , res) {
 
 function search (req, res) {
 	var searchQuery = req.params.searchTerm
-	// console.log(process.env.GBAPIK)
 	// use 'request' module to query API, reply with an error or any games that are found.
 	request('http://www.giantbomb.com/api/search/?api_key='+process.env.GBAPIK+'&format=json&query="'+searchQuery+'"&resources=game', 
 		req_options,
 		function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
-		  	// console.log('success') 
 			res.json(body)
 			}else{
-		  	// console.log('error')
-		  	// console.log(body);
 		  	res.status(500).json(error)
-
 		  }
 		}
 	)
@@ -35,12 +30,9 @@ function showGame (req, res) {
 		req_options,
 		function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
-		  	// console.log('success')
 		    console.log('IT WORKED: '+body) // Show the JSON of the search query. 
 			res.json(body)
 			}else{
-		  	// console.log('error')
-		  	// console.log(body);
 		  	res.status(500).json(error)
 
 		  }
