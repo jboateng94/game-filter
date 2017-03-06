c79fa9bd8d2ae8e1f4a5733010c9ed03e7266686
Jeremy Boateng <jeremyba@spartaglobal.co>
2017-03-06 15:03:44 +0000

    fixed bug where input fields on login/signup wouldn't change when switching between the two pages

--- models/user.js
+++ models/user.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
   uid: {type: String, required: true, unique: true},
-  favourites:[{type: String, unique: true}]
+  favourites:[{type: String, unique: true}],
+  psn: {type: String},
+  xbl: {type: String},
+  steam: {type: String}
 })
 
 module.exports = mongoose.model('User', UserSchema);
